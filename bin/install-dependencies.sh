602b7cbc3ec4ec5133d3c5057573df69cf3fcb44
C-Alexander <alexander@teamcorgi.com>
2017-10-09 22:06:27 +0200

    sudo it all.. seems secure!

--- bin/install-dependencies.sh
+++ bin/install-dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /tmp/
 
-curl --silent --location https://rpm.nodesource.com/setup_6.x | bash -
+sudo curl --silent --location https://rpm.nodesource.com/setup_6.x | bash -
 sudo yum install -y nodejs npm > ./log
 
 sudo npm update
