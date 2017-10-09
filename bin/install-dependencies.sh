0a9a124a5072f180165145e89905cb8c44144dbf
C-Alexander <alexander@teamcorgi.com>
2017-10-09 22:11:40 +0200

    OK only needed one sudo...wrestler!

--- bin/install-dependencies.sh
+++ bin/install-dependencies.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /tmp/
 
-sudo curl --silent --location https://rpm.nodesource.com/setup_6.x | bash -
-sudo yum install -y nodejs npm > ./log
+curl --silent --location https://rpm.nodesource.com/setup_6.x | sudo bash -
+yum install -y nodejs npm > ./log
 
-sudo npm update
+npm update
