2d0cf9c031140fce7e9c2cb7c27035001877cf2a
DragonicNick <nickoorthuizen@gmail.com>
2017-10-12 21:39:05 +0200

    remade update playlist database

--- routes/api/playlists.ts
+++ routes/api/playlists.ts
@@ -9,7 +9,21 @@ const playlist = require('../../models/playlist');
 
 /* POST playlist. */
 router.post('/', oauth.authenticate({scope:"Indexer"}), function(req, res, next) {
-    playlist.saveOrUpdatePlaylist(req.body.name, req.body.songs, res.locals.oauth.token.user.id, req.body.playlisttype, req.body.startdatetime, req.body.enddatetime, function(error, result){
+    playlist.savePlaylist(req.body.name, req.body.songs, res.locals.oauth.token.user.id, req.body.playlisttype, req.body.startdatetime, req.body.enddatetime, function(error, result){
+        if (error) res.send('false');
+        else res.send(result);
+    })
+});
+
+router.put('/', oauth.authenticate({scope:"Indexer"}), function(req, res, next) {
+    playlist.updatePlaylist(req.body.name, req.body.songs, res.locals.oauth.token.user.id, req.body.playlisttype, req.body.startdatetime, req.body.enddatetime, function(error, result){
+        if (error) res.send('false');
+        else res.send(result);
+    })
+});
+
+router.del('/', oauth.authenticate({scope:"Indexer"}), function(req, res, next) {
+    playlist.deletePlaylist(req.body.name, req.body.songs, res.locals.oauth.token.user.id, req.body.playlisttype, req.body.startdatetime, req.body.enddatetime, function(error, result){
         if (error) res.send('false');
         else res.send(result);
     })
