91b23b8e92b7b90f03b594f447b56460410afd38
Alexander Vegt <alexander@contritio.com>
2017-10-17 02:18:28 +0200

    Removed some old, bad code

--- routes/api/playlists.ts
+++ routes/api/playlists.ts
@@ -9,21 +9,20 @@ const playlist = require('../../models/playlist');
 
 /* POST playlist. */
 router.post('/', oauth.authenticate({scope:"Indexer"}), function(req, res, next) {
-    console.log('got here');
     playlist.savePlaylist(req.body.name, req.body.songs, res.locals.oauth.token.user.id, req.body.playlisttype, req.body.startdatetime, req.body.enddatetime, function(error, result){
         if (error) res.send('false');
         else res.send(result);
     })
 });
 
-router.put('/', oauth.authenticate({scope:"Indexer"}), function(req, res, next) {
+router.put('/', oauth.authenticate({scope:"Indexer"}), function(req, res) {
     playlist.updatePlaylist(req.body.name, req.body.songs, res.locals.oauth.token.user.id, req.body.playlisttype, req.body.startdatetime, req.body.enddatetime, function(error, result){
         if (error) res.send('false');
         else res.send(result);
     })
 });
 
-router.delete('/', oauth.authenticate({scope:"Indexer"}), function(req, res, next) {
+router.delete('/', oauth.authenticate({scope:"Indexer"}), function(req, res) {
     playlist.deletePlaylist(req.body.name, req.body.songs, res.locals.oauth.token.user.id, req.body.playlisttype, req.body.startdatetime, req.body.enddatetime, function(error, result){
         if (error) res.send('false');
         else res.send(result);
